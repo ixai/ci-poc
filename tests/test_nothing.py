61db19aaa0b17d2d56b79d3137ed5707c667c7f9
Ixai Lanzagorta <ixai@wizeline.com>
2018-03-07 16:38:21 -0600

    :construction: Fail unit testing

--- tests/test_nothing.py
+++ tests/test_nothing.py
@@ -4,3 +4,8 @@ from unittest import TestCase
 class TestNothing(TestCase):
     def test_nothing(self):
         assert True
+
+
+class TestFail(TestCase):
+    def test_fail(self):
+        assert False
